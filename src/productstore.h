7a6429c16ae1dcc4420ada6203f2b524495c7832
Mathew <N1192518@my.ntu.ac.uk>
2026-03-04 18:08:39 +0000

    Function to enable products to be edited

--- src/productstore.h
+++ src/productstore.h
@@ -13,6 +13,10 @@ public:
     bool addProduct(const Product& p);
     const std::vector<Product>& list() const;
     bool removeProduct(const std::string& id);
+    bool updateProduct(const std::string& id,
+                       const std::string& name,
+                       int quantity,
+                       const std::string& location);
 
     void loadFromFile(const std::string& filename);
     void saveToFile(const std::string& filename) const;
