d4fb47da5f13f41a660c7770ff397348e51a2581
Mathew <N1192518@my.ntu.ac.uk>
2026-03-06 14:11:50 +0000

    tells the compiler that ProductStore has a search function that returns a list of matching products

--- src/productstore.h
+++ src/productstore.h
@@ -17,6 +17,7 @@ public:
                        const std::string& name,
                        int quantity,
                        const std::string& location);
+    std::vector<Product> searchProducts(const std::string& term) const;
 
     void loadFromFile(const std::string& filename);
     void saveToFile(const std::string& filename) const;
